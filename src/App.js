3a7b79fc7b8f7b22c1821f87f8c861105673743d
tarkesh2shar <tarkesh2shar@gmail.com>
2018-10-11 06:36:24 +0530

    sda

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Route,Switch } from "react-router-dom";
+import Dummy1 from './components/dummy';
+import Dummy2 from './components/dummy2';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+export default class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <BrowserRouter>
+
+                <Switch>
+
+                <Route path="/dummy1" component={Dummy1}/>
+                <Route path="/" component={Dummy2}/>
+                </Switch>
+                
+                </BrowserRouter>
+            </div>
+        );
+    }
+
 }
-
-export default App;
